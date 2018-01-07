d3952eace6b99405976f28b28e818f3917774bc8
stefan-zobel <stefan-zobel@users.noreply.github.com>
2018-01-07 22:51:15 +0100

    Bump jsr166 CVS rev. no [no streamsupport relevant changes]

--- src/main/java/java8/util/COWArrayListSpliterator.java
+++ src/main/java/java8/util/COWArrayListSpliterator.java
@@ -28,7 +28,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 // Spliterator for java.util.concurrent.CopyOnWriteArrayList
 final class COWArrayListSpliterator {
-// CVS rev. 1.144
+// CVS rev. 1.146
     private COWArrayListSpliterator() {
     }
 
