aeb8762a6da7085b1eb5d0ffa326e02e2cfada88
stefan-zobel <stefan-zobel@users.noreply.github.com>
2018-07-24 15:17:42 +0200

    Fix errorprone warning [AssertionFailureIgnored]

--- src/tests/java/org/openjdk/tests/tck/ForkJoinPool8Test.java
+++ src/tests/java/org/openjdk/tests/tck/ForkJoinPool8Test.java
@@ -25,7 +25,7 @@ import junit.framework.TestSuite;
 
 @org.testng.annotations.Test
 public class ForkJoinPool8Test extends JSR166TestCase {
-// CVS rev. 1.39
+// CVS rev. 1.40
 
 //    public static void main(String[] args) {
 //        main(suite(), args);
@@ -114,10 +114,13 @@ public class ForkJoinPool8Test extends JSR166TestCase {
         assertNull(a.join());
         assertFalse(a.cancel(false));
         assertFalse(a.cancel(true));
+        Object v1 = null, v2 = null;
         try {
-            assertNull(a.get());
-            assertNull(a.get(randomTimeout(), randomTimeUnit()));
+            v1 = a.get();
+            v2 = a.get(randomTimeout(), randomTimeUnit());
         } catch (Throwable fail) { threadUnexpectedException(fail); }
+        assertNull(v1);
+        assertNull(v2);
     }
 
     void checkCancelled(ForkJoinTask<?> a) {
