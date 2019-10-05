63a1f942780e92da93192d4cffc820a53bad5d39
stefan-zobel <stefan-zobel@users.noreply.github.com>
2019-10-05 15:56:08 +0200

    JDK-8231161: Wrong return type in Collector Javadoc code sample

--- src/main/java/java8/util/stream/Collector.java
+++ src/main/java/java8/util/stream/Collector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -153,7 +153,7 @@ import java8.util.function.Supplier;
  * Performing a reduction operation with a {@code Collector} should produce a
  * result equivalent to:
  * <pre>{@code
- *     R container = collector.supplier().get();
+ *     A container = collector.supplier().get();
  *     for (T t : data)
  *         collector.accumulator().accept(container, t);
  *     return collector.finisher().apply(container);
