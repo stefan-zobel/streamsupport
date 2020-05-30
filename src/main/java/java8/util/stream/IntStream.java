9fed69838cccc2fb7e7a679ce11f54f08b3f6138
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-05-30 16:17:00 +0200

    JDK-8245970: IntStream.reduce Javadoc should not mention average

--- src/main/java/java8/util/stream/IntStream.java
+++ src/main/java/java8/util/stream/IntStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -457,8 +457,9 @@ public interface IntStream extends BaseStream<Integer, IntStream> {
      * <p>This is a <a href="package-summary.html#StreamOps">terminal
      * operation</a>.
      *
-     * <p><b>API Note:</b><br> Sum, min, max, and average are all special cases of reduction.
-     * Summing a stream of numbers can be expressed as:
+     * <p><b>API Note:</b><br> Sum, min and max are all special cases of
+     * reduction that can be expressed using this method.
+     * For example, summing a stream can be expressed as:
      *
      * <pre>{@code
      *     int sum = integers.reduce(0, (a, b) -> a+b);
