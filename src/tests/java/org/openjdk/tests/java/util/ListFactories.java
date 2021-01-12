6d44040dd741c21e71153ab6901104a586290d30
stefan-zobel <stefan-zobel@users.noreply.github.com>
2021-01-12 14:30:42 +0100

    JDK-8180352: Add Stream.toList() method

--- src/tests/java/org/openjdk/tests/java/util/ListFactories.java
+++ src/tests/java/org/openjdk/tests/java/util/ListFactories.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -37,6 +37,7 @@ import java.util.ListIterator;
 
 import java8.util.Iterators;
 import java8.util.Lists;
+import java8.util.stream.RefStreams;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -361,6 +362,20 @@ public class ListFactories {
     public void copyOfRejectsNullElements() {
         @SuppressWarnings("unused")
         List<Integer> list = Lists.copyOf(Arrays.asList(1, null, 3));
+    }
+
+    @Test(expectedExceptions=NullPointerException.class)
+    public void copyOfRejectsNullElements2() {
+        @SuppressWarnings("unused")
+        List<String> list = Lists.copyOf(RefStreams.of("a", null, "c").toList());
+    }
+
+    @Test
+    public void copyOfCopiesNullAllowingList() {
+        List<String> orig = RefStreams.of("a", "b", "c").toList();
+        List<String> copy = Lists.copyOf(orig);
+
+        assertNotSame(orig, copy);
     }
 
     @Test
