3d822dc4fd745004083685be670585b50353dbcb
stefan-zobel <stefan-zobel@users.noreply.github.com>
2021-01-19 22:13:28 +0100

    JDK-8259796: timed CompletableFuture.get swallows InterruptedException

--- src/cfuture/java/java8/util/concurrent/CompletableFuture.java
+++ src/cfuture/java/java8/util/concurrent/CompletableFuture.java
@@ -139,7 +139,7 @@ import java8.util.stream.StreamSupport;
  * @since 1.8
  */
 public class CompletableFuture<T> implements Future<T>, CompletionStage<T> {
-// CVS rev. 1.222
+// CVS rev. 1.225
     /*
      * Overview:
      *
@@ -1958,14 +1958,15 @@ public class CompletableFuture<T> implements Future<T>, CompletionStage<T> {
             if (r == null)
                 cleanStack();
         }
-        if (r == null && !interrupted)
-            throw new TimeoutException();
-        else if (r != null) {
+        if (r != null) {
             if (interrupted)
                 Thread.currentThread().interrupt();
             postComplete();
-        }
-        return r;
+            return r;
+        } else if (interrupted)
+            return null;
+        else
+            throw new TimeoutException();
     }
 
     /* ------------- public methods -------------- */
