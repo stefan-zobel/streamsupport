682944416211d7ccd669f062d1605a951ade3d95
stefan-zobel <stefan-zobel@users.noreply.github.com>
2021-05-23 15:07:47 +0200

    Avoid prolongation of lifetime when iteration is finished

--- src/main/java/java8/util/Spliterators.java
+++ src/main/java/java8/util/Spliterators.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2016 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2021 Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1383,7 +1383,9 @@ public final class Spliterators {
                     throw new NoSuchElementException();
                 else {
                     valueReady = false;
-                    return nextElement;
+                    T t = nextElement;
+                    nextElement = null;
+                    return t;
                 }
             }
 
