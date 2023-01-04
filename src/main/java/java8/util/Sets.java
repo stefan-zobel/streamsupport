d42b5632d38f31553c0cc11eb4e9908d46b11c2c
stefan-zobel <stefan-zobel@users.noreply.github.com>
2023-01-04 17:34:27 +0000

    JDK-8299444

--- src/main/java/java8/util/Sets.java
+++ src/main/java/java8/util/Sets.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -351,6 +351,8 @@ public final class Sets {
     public static <E> Set<E> copyOf(Collection<? extends E> coll) {
         if (coll instanceof ImmutableCollections.AbstractImmutableSet) {
             return (Set<E>) coll;
+        } else if (coll.isEmpty()) {
+            return Sets.of();
         } else {
             return (Set<E>) Sets.of(new HashSet<>(coll).toArray());
         }
