3a755f271ed7b27a5f2f94ca5a2eaf5910f004c4
stefan-zobel <stefan-zobel@users.noreply.github.com>
2023-01-10 00:09:32 +0100

    JDK-8299444

--- src/main/java/java8/util/ImmutableCollections.java
+++ src/main/java/java8/util/ImmutableCollections.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -133,6 +133,8 @@ final class ImmutableCollections {
     static <E> List<E> listCopy(Collection<? extends E> coll) {
         if (coll instanceof List12 || (coll instanceof ListN && ! ((ListN<?>) coll).allowNulls)) {
             return (List<E>) coll;
+        } else if (coll.isEmpty()) { // implicit nullcheck of coll
+            return Lists.of();
         } else {
             return (List<E>) Lists.of(coll.toArray()); // implicit nullcheck of coll
         }
